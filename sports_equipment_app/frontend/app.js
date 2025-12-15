dfd7a56286e302eda6f60efd86a024e1d6d8cec6
bmahe1 <maheshreddy3585@gmail.com>
2025-12-15 12:20:12 +0530

    Update app.js

--- sports_equipment_app/frontend/app.js
+++ sports_equipment_app/frontend/app.js
@@ -1,13 +1,1 @@
-fetch("http://Docker-env.eba-qame2mxj.us-east-1.elasticbeanstalk.com/products")
-    .then(response => response.json())
-    .then(products => {
-        const list = document.getElementById("product-list");
-        list.innerHTML = products.map(product => `
-            <div class='card'>
-                <img src="${product.image}" alt="${product.name}" />
-                <h3>${product.name}</h3>
-                <p>Price: $${product.price}</p>
-            </div>
-        `).join("");
-    })
-    .catch(err => console.error("Failed to load products", err));
+fetch("products.json") .then(response => response.json()) .then(products => { const list = document.getElementById("product-list"); list.innerHTML = products.map(product => <div class='card'> <img src="${product.image}" alt="${product.name}" /> <h3>${product.name}</h3> <p>Price: $${product.price}</p> </div> ).join(""); }) .catch(err => console.error("Failed to load products", err));
