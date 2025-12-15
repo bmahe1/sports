31f2229f1e7c36e0c6dd88406dc5d54bd386d379
bmahe1 <maheshreddy3585@gmail.com>
2025-12-15 12:32:49 +0530

    Update app.js

--- sports_equipment_app/frontend/app.js
+++ sports_equipment_app/frontend/app.js
@@ -1,1 +1,13 @@
-fetch("products.json") .then(response => response.json()) .then(products => { const list = document.getElementById("product-list"); list.innerHTML = products.map(product => <div class='card'> <img src="${product.image}" alt="${product.name}" /> <h3>${product.name}</h3> <p>Price: $${product.price}</p> </div> ).join(""); }) .catch(err => console.error("Failed to load products", err));
+fetch("http://54.152.33.30:8080/products") // or EB environment URL
+    .then(response => response.json())
+    .then(products => {
+        const list = document.getElementById("product-list");
+        list.innerHTML = products.map(product => `
+            <div class='card'>
+                <img src="${product.image}" alt="${product.name}" />
+                <h3>${product.name}</h3>
+                <p>Price: $${product.price}</p>
+            </div>
+        `).join("");
+    })
+    .catch(err => console.error("Failed to load products", err));
